68269106fd3726e1d71fd133c258df93894928ea
Pedro Reis <pedroandradereis2007@gmail.com>
2022-12-27 14:54:16 -0300

    modified:   src/util/others/mstohours.js

--- src/util/others/mstohours.js
+++ src/util/others/mstohours.js
@@ -2,6 +2,7 @@ module.exports = function mstohours(duration) {
   duration /= 1000;
   duration = duration.toFixed(0);
 
+  let days = 0
   let hours = 0
   let minutes = 0
   let seconds = duration
@@ -13,11 +14,17 @@ module.exports = function mstohours(duration) {
       hours += 1
       minutes -= 60
   }
+  while (hours >= 24) {
+    days += 1
+    hours -= 60
+}
   if (minutes < 10) minutes = `0${minutes}`
   if (seconds < 10) seconds = `0${seconds}`
 
-  if (hours > 0)
-      return `${hours}:${minutes}:${seconds} horas.`
+  if (days > 0)
+      return `${days} dias, ${hours} horas, ${minutes} minutos e ${seconds} segundos.`
+  else if (hours > 0)
+      return `${hours} horas, ${minutes} minutos e ${seconds} segundos.`
   else
       return `${minutes}:${seconds} minutos.`
 }
